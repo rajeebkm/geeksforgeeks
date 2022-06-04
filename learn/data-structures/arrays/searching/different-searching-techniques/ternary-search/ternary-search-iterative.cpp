d8cd0620db2832ed6e597e58e043460877f106b9
rajeebkm <rkm2541997@gmail.com>
2022-06-04 20:57:59 +0530

    Ternary search added

---
+++ learn/data-structures/arrays/searching/different-searching-techniques/ternary-search/ternary-search-iterative.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+#include<algorithm>
+
+using namespace std;
+
+int ternarySearchIterative(int arr[], int l, int r, int key){
+
+   
+
+    while (l<=r){
+        int mid1 = l + (r - l) / 3;
+        int mid2 = r - (r - l) / 3;
+
+        if (arr[mid1]==key){
+            return mid1;
+        }
+
+        if(arr[mid2]==key){
+            return mid2;
+        }
+        
+        if(key<arr[mid1]){
+            r = mid1 - 1;
+        }else if(key>arr[mid2]){
+            l = mid2 + 1;
+        }else {
+            l = mid1 + 1;
+            r = mid2 - 1;
+        }
+    }
+
+    return -1;
+}
+
+int main(){
+    int array[] = {2, 6, 4, 5, 90, 34, 27};
+    int arr_size = *(&array + 1) - array;
+       sort(array, array + arr_size);
+
+    cout << "\nSorted array: ";
+    for (int i = 0; i < arr_size; i++){
+        cout << array[i] << ", ";
+    }
+
+    int key;    
+
+	while (true) 
+	{
+        cout << "\nPlease enter the value, you want index: ";
+        if(!(cin>>key))
+        {
+   		    cin.clear();
+		    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Invalid integer!!! Please try again\n\n";
+            continue;
+        }
+        else 
+            break;
+    }
+
+    cout << "The value you entered is: " << key;
+
+    int l = 0;
+    int r = arr_size - 1;
+    int index = ternarySearchIterative(array, l, r, key);
+
+    if(index!=-1){
+        cout << "\nValue found at the Index: " << index <<"\n";
+    }else{
+        cout << "\nValue is not found.\n";
+    }
+
+        return 0;
+}
---
+++ learn/data-structures/arrays/searching/different-searching-techniques/ternary-search/ternary-search-recursive.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+#include<algorithm>
+
+using namespace std;
+
+int ternarySearchRecursive(int arr[], int l, int r, int key){
+
+   
+
+    while (l<=r){
+        int mid1 = l + (r - l) / 3;
+        int mid2 = r - (r - l) / 3;
+
+        if (arr[mid1]==key){
+            return mid1;
+        }
+
+        if(arr[mid2]==key)
+            return mid2;
+    
+        if(key<arr[mid1]){
+            return ternarySearchRecursive(arr, l, mid1 - 1, key);
+        }else if(key>arr[mid2]){
+            return ternarySearchRecursive(arr, mid2 + 1, r, key);
+        }else{
+            return ternarySearchRecursive(arr, mid1, mid2 - 1, key);
+        }
+            
+    }
+
+    return -1;
+}
+
+int main(){
+    int array[] = {2, 6, 4, 5, 90, 34, 27};
+    int arr_size = *(&array + 1) - array;
+
+    int key = 4;
+
+    sort(array, array + arr_size);
+
+    cout << "Sorted array: " << endl;
+    for (int i = 0; i < arr_size; i++){
+        cout << array[i] << ", ";
+    }
+
+    int l = 0;
+    int r = arr_size - 1;
+
+    cout << "Value found at the Index: " << ternarySearchRecursive(array, l, r, key);
+
+    return 0;
+}
