75b9079b4e190b171149929ff38541f6e751c758
rajeebkm <rkm2541997@gmail.com>
2022-06-07 11:34:14 +0530

    Print matric in clockwise spiral form

--- learn/data-structures/matrix/traversal-in-matrix/print-matrix-in-spiral-form/print-matrix-in-spiral-form.cpp
+++ learn/data-structures/matrix/traversal-in-matrix/print-matrix-in-spiral-form/print-matrix-in-spiral-form.cpp
@@ -5,7 +5,7 @@ using namespace std;
 #define R 4
 #define C 4
 
-void spiralPrint(int m, int n, int a[R][C])
+void spiralClockcwisePrint(int m, int n, int a[R][C])
 {
 	int i, k = 0, l = 0;
 
@@ -27,27 +27,27 @@ void spiralPrint(int m, int n, int a[R][C])
 		/* Print the last column
 		from the remaining columns */
 		for (i = k; i < m; ++i) {
-			cout << a[i][n - 1] << " ";
+			cout << a[i][n-1] << " ";
 		}
 		n--;
 
 		/* Print the last row from
 				the remaining rows */
-		if (k < m) {
-			for (i = n - 1; i >= l; --i) {
-				cout << a[m - 1][i] << " ";
+		// if (k < m) {
+			for (i = n-1; i >= l; --i) {
+				cout << a[m-1][i] << " ";
 			}
 			m--;
-		}
+		// }
 
 		/* Print the first column from
 				the remaining columns */
-		if (l < n) {
+		// if (l < n) {
 			for (i = m - 1; i >= k; --i) {
 				cout << a[i][l] << " ";
 			}
 			l++;
-		}
+		// }
 	}
 }
 
@@ -55,12 +55,12 @@ void spiralPrint(int m, int n, int a[R][C])
 int main()
 {
 	int a[R][C] = {{1, 2, 3, 4},
-				{5, 6, 7, 8},
-				{9, 10, 11, 12},
-				{13, 14, 15, 16}};
+				   {5, 6, 7, 8},
+				   {9, 10, 11, 12},
+				   {13, 14, 15, 16}};
 
 	// Function Call
-	spiralPrint(R, C, a);
+	spiralClockcwisePrint(R, C, a);
 	return 0;
 }
 
