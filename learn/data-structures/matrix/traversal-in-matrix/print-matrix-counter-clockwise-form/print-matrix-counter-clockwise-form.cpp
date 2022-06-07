2c3806ac1a05b078b73703017da5aa2fabb0ee6a
rajeebkm <rkm2541997@gmail.com>
2022-06-07 11:35:04 +0530

    Print matric in counter clockwise spiral form

--- learn/data-structures/matrix/traversal-in-matrix/print-matrix-counter-clockwise-form/print-matrix-counter-clockwise-form.cpp
+++ learn/data-structures/matrix/traversal-in-matrix/print-matrix-counter-clockwise-form/print-matrix-counter-clockwise-form.cpp
@@ -1,47 +1,67 @@
-#include<bits/stdc++.h>
+// C++ Program to print a matrix spirally
+
+#include <bits/stdc++.h>
 using namespace std;
+#define R 4
+#define C 4
 
-#define R 4;
-#define C 4;
+void spiralCounterClockwisePrint(int m, int n, int a[R][C])
+{
+	int i, k = 0, l = 0;
 
-void spiralCounterClockwisePrint(int m, int n, int a[R][C]){
+	/* k - starting row index
+		m - ending row index
+		l - starting column index
+		n - ending column index
+		i - iterator
+	*/
 
-    int i, k = 0, l = 0;
+	while (k < m && l < n) {
+		/* Print the first column from
+			the remaining columns */
+		for (i = k; i < m; ++i) {
+			cout << a[i][l] << " ";
+		}
+		l++;
 
-    while(k<m && l<n){
-        for (i = l; i < n; ++i){
-            cout << a[k][i] << " ";
-        }
-        k++;
+		/* Print the last row
+		from the remaining rows */
+		for (i = l; i < n; ++i) {
+			cout << a[m-1][i] << " ";
+		}
+		m--;
 
-        for (i = k; i < m; ++i){
-            cout << a[i][n-1] << " ";
-        }
-        n--;
+		/* Print the last column from
+				the remaining columns */
+		// if (k < m) {
+			for (i = m-1; i >= k; --i) {
+				cout << a[i][n-1] << " ";
+			}
+			n--;
+		// }
 
-        if(k<m){
-            for (i = n - 1; i>=l; --i){
-                cout << a[m - 1][i] << " ";
-            }
-            --m;
-        }
-        if (l < n){
-                for (i = m - 1; i >= k; --i){
-                    cout << a[i][l] << " ";
-                }
-                l++;
-            }
-    }
+		/* Print the first row from
+				the remaining rows */
+		// if (l < n) {
+			for (i = n - 1; i >= l; --i) {
+				cout << a[k][i] << " ";
+			}
+			k++;
+		// }
+	}
 }
 
+/* Driver Code */
 int main()
 {
 	int a[R][C] = {{1, 2, 3, 4},
-				{5, 6, 7, 8},
-				{9, 10, 11, 12},
-				{13, 14, 15, 16}};
+				   {5, 6, 7, 8},
+				   {9, 10, 11, 12},
+				   {13, 14, 15, 16}};
 
 	// Function Call
 	spiralCounterClockwisePrint(R, C, a);
 	return 0;
 }
+
+
